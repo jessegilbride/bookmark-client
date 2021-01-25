bb10d80c274ba7da292d6313ce9a9ee18df725e7
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-01-25 09:30:23 -0500

    swap the page management using state for Routes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  // API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$wcrZ3Ot1HUycCuZLdnTiDu2YmpmnywtxjaN6dz0S5U765AHRof2.u',
 }
